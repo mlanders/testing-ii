49f7199b2ddc326ae9b8f892c44559c3885ff7f2
Mike Landers <landers.mike@gmail.com>
2019-03-05 12:37:03 -0800

    adjusted repo setup

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,9 @@ import { render } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import App from './App';
 
-it('renders without crashing', () => {
-	render(<App />);
-});
+describe('<App/>', () => {
+	it('renders without crashing', () => {
+		render(<App />);
+	});
+	
+})
